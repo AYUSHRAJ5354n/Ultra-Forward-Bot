eb144d53922fe8778b7c0e233e1c1b4c478a2409
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2024-12-31 00:47:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,18 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
-
 import asyncio
-import logging 
+import logging
 import logging.config
-from database import db 
-from config import Config  
+from database import db
+from config import Config
 from pyrogram import Client, __version__
-from pyrogram.raw.all import layer 
+from pyrogram.raw.all import layer
 from pyrogram.enums import ParseMode
-from pyrogram.errors import FloodWait 
+from pyrogram.errors import FloodWait
 
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
-class Bot(Client): 
+class Bot(Client):
     def __init__(self):
         super().__init__(
             Config.BOT_SESSION,
@@ -48,38 +39,23 @@ class Bot(Client):
         success = failed = 0
         users = await db.get_all_frwd()
         async for user in users:
-           chat_id = user['user_id']
-           try:
-              await self.send_message(chat_id, text)
-              success += 1
-           except FloodWait as e:
-              await asyncio.sleep(e.value + 1)
-              await self.send_message(chat_id, text)
-              success += 1
-           except Exception:
-              failed += 1 
-    #    await self.send_message("venombotsupport", text)
+            chat_id = user['user_id']
+            try:
+                await self.send_message(chat_id, text)
+                success += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.value + 1)
+                await self.send_message(chat_id, text)
+                success += 1
+            except Exception:
+                failed += 1
         if (success + failed) != 0:
-           await db.rmve_frwd(all=True)
-           logging.info(f"Restart message status"
-                 f"success: {success}"
-                 f"failed: {failed}")
+            await db.rmve_frwd(all=True)
+            logging.info(f"Restart message status"
+                  f"success: {success}"
+                  f"failed: {failed}")
 
     async def stop(self, *args):
         msg = f"@{self.username} stopped. Bye."
         await super().stop()
         logging.info(msg)
-
-
-
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
