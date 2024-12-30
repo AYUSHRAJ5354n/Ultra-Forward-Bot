c4ba0474634cb299abff8e84d5806d3f07947ff3
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2024-12-31 00:22:36 +0530

    Update set_profile_photo.py

--- set_profile_photo.py
+++ set_profile_photo.py
@@ -1,7 +1,7 @@
 from typing import Union, List
 
 class SetProfilePhoto:
-    def __init__(self, emoji_background: Union[int, List[int]] = None):
+    def __init__(self, emoji_background: Union[int, List[int]] = None
         self.emoji_background = emoji_background
 
     def set_photo(self, photo):
