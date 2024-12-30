7201548031054b7d8a648ec3081c2a0192ae1dd1
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2024-12-31 00:16:51 +0530

    Create set_profile_photo.py

---
+++ set_profile_photo.py
@@ -0,0 +1,9 @@
+from typing import Union, List
+
+class SetProfilePhoto:
+    def __init__(self, emoji_background: Union[int, List[int]] = None):
+        self.emoji_background = emoji_background
+
+    def set_photo(self, photo):
+        # Implement the logic to set the profile photo
+        pass
